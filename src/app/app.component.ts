eb9038c7fd3d9adf4e241b9f8f290ec344deba1a
DevNand0 <fernandoo.avellaneda.m@gmail.com>
2020-09-26 01:45:55 -0500

    validacion de react forms, creacion de elementos con funciones de tipo get para la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { PasswordValidator } from './shared/password.validator';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ReactForms';
+export class AppComponent implements OnInit{
+  registrationForm: FormGroup;
+
+  //pasa ese atributo para los validadores de input nombre
+  get nombre(){
+    return this.registrationForm.get('nombre');
+  }
+
+  get email(){
+    return this.registrationForm.get('email');
+  }
+
+  constructor(private fb: FormBuilder){}
+
+    ngOnInit(){
+      this.registrationForm = this.fb.group({
+        nombre: ['',[Validators.required, Validators.minLength(4)]],
+        password: [''],
+        confirmPassword: [''],
+        email: [''],
+        subscribe: [false],
+        address: this.fb.group({
+          ciudad: [''],
+          provincia: [''],
+          postal: [''],
+        })
+      },{ validator: PasswordValidator })
+      
+      this.registrationForm.get('subscribe').valueChanges.subscribe(checkedValue => {
+        const email = this.registrationForm.get('email');
+        if(checkedValue){
+          email.setValidators(Validators.required);
+        }else{
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      });
+    }
+    
+  
+
+  loadApiData(){
+
+    this.registrationForm.patchValue({
+      nombre: 'Bruce',
+      address:{
+        ciudad: 'Genova',
+      }
+    });
+  }
 }
